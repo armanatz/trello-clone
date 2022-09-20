d8252a8993f5b829e62b885098bdee46ee565a6f
Arman Attarzadeh <arman.atz@gmail.com>
2022-09-20 09:47:39 +0000

    Add project provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import { ProjectProvider } from './contexts/Project';
+
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <main>
+        <ProjectProvider>
+          <div></div>
+        </ProjectProvider>
+      </main>
     </div>
   );
 }
