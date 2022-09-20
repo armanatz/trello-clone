2e45e004833dc623b233ab0f0108a149c3748e15
Arman Attarzadeh <arman.atz@gmail.com>
2022-09-20 09:34:01 +0000

    Add navbar component

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,15 @@
+import './style.css';
+
+const NavBar = () => {
+  return (
+    <header className="nav">
+      <div className="container">
+        <div className="nav-logo">
+          <h1>InstrelloPro</h1>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
